fcf39c4e6bd2af8fb15e55ea43366c16d18d78fb
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-03-01 17:17:13 +0200

    Added logout functionality

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from user.serializers import RegisterSerializer
@@ -26,3 +27,19 @@ class RegisterView(generics.CreateAPIView):
         }
 
         return Response(response_data, status=status.HTTP_201_CREATED)
+
+class LogoutView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        try:
+            refresh_token = request.data.get("refresh")
+            if not refresh_token:
+                return Response({"error": "Refresh token is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response({"message": "Logout successful"}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
