5d4305c9385c786d28224a84fc8cb9b8bf1f9fd8
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-03-03 00:02:01 +0200

    Removed websocket setup it will be migrated to node websocket service

--- backend/debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "🐛 Starting Uvicorn in Debug Mode..."
-uvicorn backend.asgi:application --host 127.0.0.1 --port 8000 --reload --log-level debug
--- backend/dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "🚀 Starting Uvicorn..."
-uvicorn backend.asgi:application --host 127.0.0.1 --port 8000 --workers 4
